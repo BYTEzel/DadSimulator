075d5343a9d16d0e0251cdb9ba9cbe52f9924036
julia <julia@DESKTOP-5A69506>
2021-09-18 18:50:10 +0200

    Add project files.

---
+++ DadSimulator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DadSimulator
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new DadSimulator())
+                game.Run();
+        }
+    }
+}
