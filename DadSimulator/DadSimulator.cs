f93657148ee08dee0fe1ddf104ceaa910873e42e
Julian <julian@heitmann-familie.de>
2021-09-19 13:02:00 +0200

    - char icon added - movement added - fullscreen

--- DadSimulator/DadSimulator.cs
+++ DadSimulator/DadSimulator.cs
@@ -6,12 +6,16 @@ namespace DadSimulator
 {
     public class DadSimulator : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager m_graphics;
+        private SpriteBatch m_spriteBatch;
+        private Texture2D m_char;
+        private Vector2 m_charPosition;
+        private float m_speed;
 
         public DadSimulator()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            m_graphics = new GraphicsDeviceManager(this);
+            m_graphics.IsFullScreen = true;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -20,18 +24,38 @@ namespace DadSimulator
         {
             // TODO: Add your initialization logic here
 
+            m_charPosition = new Vector2(
+                m_graphics.PreferredBackBufferWidth / 2,
+                m_graphics.PreferredBackBufferHeight / 2);
+            m_speed = 100f;
+
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            m_spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            m_char = Content.Load<Texture2D>("Smiley");
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            var kstate = Keyboard.GetState();
+
+            if (kstate.IsKeyDown(Keys.Up))
+                m_charPosition.Y -= m_speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Down))
+                m_charPosition.Y += m_speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Left))
+                m_charPosition.X -= m_speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Right))
+                m_charPosition.X += m_speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
@@ -42,9 +66,14 @@ namespace DadSimulator
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+            m_spriteBatch.Begin();
+            m_spriteBatch.Draw(m_char, m_charPosition, null, 
+                Color.White, 0f, new Vector2(m_char.Width / 2, m_char.Height / 2), 
+                Vector2.One, SpriteEffects.None, 0f); ;
+            m_spriteBatch.End();
 
             base.Draw(gameTime);
         }
