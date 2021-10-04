cc78c9813fb87452a4790c6b10db12fb66bce395
Julian <julian@heitmann-familie.de>
2021-10-04 17:10:24 +0200

    refactored DrawRectangleInteractable to be more readable

--- DadSimulator/UI/UiEngine.cs
+++ DadSimulator/UI/UiEngine.cs
@@ -44,12 +44,34 @@ namespace DadSimulator.UI
         {
             const float scalingHeadline = 0.7f;
             const float scalingText = 0.5f;
+            const int padding = 10;
+            const int shift = 50;
 
             var textSizeHeadline = m_fontHeadline.MeasureString(headline) * scalingHeadline;
             var textSizeInBox = m_fontText.MeasureString(textInBox) * scalingText;
-            int padding = 10;
 
-            const int shift = 50;
+            Point positionTopLeft = ComputeTopLeftPosition(positionInteractable, relativePosition,
+                padding, shift, textSizeHeadline, textSizeInBox);
+
+            Rectangle rectangle = ComputeRectangle(padding, textSizeHeadline, textSizeInBox, positionTopLeft);
+
+            DrawLine(new Vector2(rectangle.Center.X, rectangle.Center.Y), positionInteractable, 5.0f, color);
+            DrawRectangle(rectangle, color);
+            DrawText(new Vector2(positionTopLeft.X + padding, positionTopLeft.Y + padding), Color.White, headline, true, scalingHeadline);
+            DrawText(new Vector2(positionTopLeft.X + padding, positionTopLeft.Y + textSizeHeadline.Y + padding), Color.White, textInBox, false, scalingText);
+        }
+
+        private static Rectangle ComputeRectangle(int padding, Vector2 textSizeHeadline, Vector2 textSizeInBox, Point positionTopLeft)
+        {
+            return new Rectangle(
+                            positionTopLeft,
+                            new Point(
+                                x: (int)System.Math.Max(textSizeHeadline.X, textSizeInBox.X) + padding * 2,
+                                y: (int)(textSizeHeadline.Y + textSizeInBox.Y) + (padding * 2)));
+        }
+
+        private static Point ComputeTopLeftPosition(Vector2 positionInteractable, RelativePosition relativePosition, int padding, int shift, Vector2 textSizeHeadline, Vector2 textSizeInBox)
+        {
             float posY;
             if (relativePosition == RelativePosition.Top)
             {
@@ -61,19 +83,9 @@ namespace DadSimulator.UI
             }
 
             var positionTopLeft = new Point(
-                x: (int)(positionInteractable.X-padding-(System.Math.Max(textSizeHeadline.X, textSizeInBox.X)/2)), 
+                x: (int)(positionInteractable.X - padding - (System.Math.Max(textSizeHeadline.X, textSizeInBox.X) / 2)),
                 y: (int)(posY));
-
-            var rectangle = new Rectangle(
-                positionTopLeft,
-                new Point(
-                    x: (int)System.Math.Max(textSizeHeadline.X, textSizeInBox.X) + padding * 2,
-                    y: (int)(textSizeHeadline.Y + textSizeInBox.Y) + (padding * 2)));
-
-            DrawLine(new Vector2(rectangle.Center.X, rectangle.Center.Y), positionInteractable, 5.0f, color);
-            DrawRectangle(rectangle, color);
-            DrawText(new Vector2(positionTopLeft.X + padding, positionTopLeft.Y + padding), Color.White, headline, true, scalingHeadline);
-            DrawText(new Vector2(positionTopLeft.X + padding, positionTopLeft.Y + textSizeHeadline.Y + padding), Color.White, textInBox, false, scalingText);
+            return positionTopLeft;
         }
 
         public void DrawText(Vector2 positionTopLeft, Color color, string text, bool isHeadline, float scaling = 1.0f)
